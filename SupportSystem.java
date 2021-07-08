0c2ef09ae444a8dba4a73f1cad34ad98dea4edd8
pkazala <40291469+pkazala@users.noreply.github.com>
2021-07-08 13:41:45 +0200

    Add files via upload

---
+++ SupportSystem.java
@@ -0,0 +1,62 @@
+import java.util.HashSet;
+
+
+ */
+public class SupportSystem
+{
+    private InputReader reader;
+    private Responder responder;
+    
+    /**
+     * Creates a technical support system.
+     */
+    public SupportSystem()
+    {
+        reader = new InputReader();
+        responder = new Responder();
+    }
+
+    /**
+     * Start the technical support system. This will print a welcome message and enter
+     * into a dialog with the user, until the user ends the dialog.
+     */
+    public void start()
+    {
+        boolean finished = false;
+
+        printWelcome();
+
+        while(!finished) {
+            HashSet<String> input = reader.getInput();
+
+            if(input.contains("bye")) {
+                finished = true;
+            }
+            else {
+                String response = responder.generateResponse(input);
+                System.out.println(response);
+            }
+        }
+        printGoodbye();
+    }
+
+    /**
+     * Print a welcome message to the screen.
+     */
+    private void printWelcome()
+    {
+        System.out.println("Welcome to the DodgySoft Technical Support System.");
+        System.out.println();
+        System.out.println("Please tell us about your problem.");
+        System.out.println("We will assist you with any problem you might have.");
+        System.out.println("Please type 'bye' to exit our system.");
+    }
+
+    /**
+     * Print a good-bye message to the screen.
+     */
+    private void printGoodbye()
+    {
+        System.out.println("Nice talking to you. Bye...");
+    }
+}
